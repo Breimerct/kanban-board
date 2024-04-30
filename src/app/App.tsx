4a5a9fb72e40fa7c2a02eee5367ac06abc82affc
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-30 01:33:25 -0500

    init project kanban board

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './App.scss';
+
+const App: FC = () => {
+   return (
+      <div>
+         <h1>Hello, World!</h1>
+      </div>
+   );
+};
+
+export default App;
