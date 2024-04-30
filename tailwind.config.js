988465b027ff0d3fea319dd289e978101ffed38d
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-30 17:51:55 -0500

    remove @material-tailwind/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
-import withMT from '@material-tailwind/react/utils/withMT';
+/** @type {import('tailwindcss').Config} */
 
-module.exports = withMT({
-   content: [
-      './index.html',
-      './src/**/*.{vue,js,ts,jsx,tsx}',
-      'node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-      'node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
-   ],
+export default {
+   darkMode: 'class',
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
-      extend: {},
+      extend: {}
    },
-   plugins: [],
-});
+   plugins: []
+};
