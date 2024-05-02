01fed78def120ebe7faf3d7b52c1c265af88106e
Breimerct98 <breimer.correa@zabud.com.co>
2024-05-01 20:21:33 -0500

    the class for the label is added when the input is invalid

--- src/assets/input/Input.tsx
+++ src/assets/input/Input.tsx
@@ -4,17 +4,20 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
    label: string;
    isError?: boolean;
    errorMessage?: string;
+   containerClassName?: string;
 }
 
 const Input: FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
-   ({ label, isError, errorMessage, className, ...props }, ref) => {
+   ({ label, isError, errorMessage, className, containerClassName, ...props }, ref) => {
       const errorClasses = isError
          ? 'border-red-500 text-red-500 focus:outline-none placeholder-red-400 placeholder:opacity-50'
          : 'text-gray-800 border-gray-700 focus:outline-gray-600';
 
+      const errorLabelClasses = isError ? 'text-red-500' : 'text-gray-800';
+
       return (
-         <label htmlFor={props.id} className="font-bold text-gray-800">
-            <span>{label}</span>
+         <label htmlFor={props.id} className={`font-bold text-gray-800 ${errorLabelClasses} ${containerClassName}`}>
+            <span className="block">{label}</span>
             <input
                {...props}
                ref={ref}
