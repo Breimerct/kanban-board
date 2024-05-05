41f80581c5520b87e07662cd5e5350dd50747f62
Breimerct98 <breimer.correa@zabud.com.co>
2024-05-05 01:35:02 -0500

    add minus icon

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -76,3 +76,20 @@ export const HomeIcon: FC<IconProps> = ({ className, size }) => (
       <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
    </svg>
 );
+
+export const MinusIcon: FC<IconProps> = ({ className, size }) => (
+   <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+   >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12l14 0" />
+   </svg>
+);
