dcd900e46e62c5b3037725940961b64b4705628e
Breimerct98 <breimer.correa@zabud.com.co>
2024-05-09 00:28:00 -0500

    Add TrashIcon component for deleting items

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -147,3 +147,24 @@ export const CheckIcon: FC<IconProps> = ({ className, size }) => (
       <path d="M5 12l5 5l10 -10" />
    </svg>
 );
+
+export const TrashIcon: FC<IconProps> = ({ className, size }) => (
+   <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+   >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 7l16 0" />
+      <path d="M10 11l0 6" />
+      <path d="M14 11l0 6" />
+      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+   </svg>
+);
