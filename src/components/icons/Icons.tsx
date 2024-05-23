1bfb05f4d491d361ed25ae46347e2ba8728042bb
Breimerct98 <breimer.correa@zabud.com.co>
2024-05-22 19:01:00 -0500

    Add new icons for logout and settings

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -239,3 +239,66 @@ export const GoogleIcon: FC<IconProps> = ({ className, size }) => (
       <path d="M20.945 11a9 9 0 1 1 -3.284 -5.997l-2.655 2.392a5.5 5.5 0 1 0 2.119 6.605h-4.125v-3h7.945z" />
    </svg>
 );
+
+export const LogoutIcon: FC<IconProps> = ({ className, size }) => (
+   <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+   >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+      <path d="M15 12h-12l3 -3" />
+      <path d="M6 15l-3 -3" />
+   </svg>
+);
+
+export const LoginIcon: FC<IconProps> = ({ className, size }) => (
+   <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+   >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+      <path d="M3 12h13l-3 -3" />
+      <path d="M13 15l3 -3" />
+   </svg>
+);
+
+export const SettingsIcon: FC<IconProps> = ({ className, size }) => (
+   <svg
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+   >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+      <path d="M6 4v4" />
+      <path d="M6 12v8" />
+      <path d="M10 16a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+      <path d="M12 4v10" />
+      <path d="M12 18v2" />
+      <path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+      <path d="M18 4v1" />
+      <path d="M18 9v11" />
+   </svg>
+);
