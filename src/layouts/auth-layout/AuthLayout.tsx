03915e8bfb30eec450592eebe22e0f7b89d386fd
Breimerct98 <breimer.correa@zabud.com.co>
2024-06-26 22:34:11 -0500

    feat: Update landing page banner text and styling

--- src/layouts/auth-layout/AuthLayout.tsx
+++ src/layouts/auth-layout/AuthLayout.tsx
@@ -1,21 +1,9 @@
-// import { useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import KanbanBoard from '../../assets/scrum_board.svg';
-// import { useAuthStore } from '../../store/auth.store';
 import { toast } from 'sonner';
 
 const AuthLayout = () => {
-   // const currentUser = useAuthStore((state) => state.currentUser);
-   // // const getUserByProvider = useAuthStore((state) => state.getUserByProvider);
-   // const navigate = useNavigate();
-
-   // useEffect(() => {
-   //    if (currentUser) {
-   //       navigate('/');
-   //    }
-   // }, [currentUser]);
-
    const handleAlert = () => {
       toast.warning('You will have limitations within the application if you have not logged in.', {
          duration: 8000,
@@ -29,7 +17,7 @@ const AuthLayout = () => {
          <header className="w-full h-full text-center">
             <Link to="/" className="transition-all" onClick={handleAlert}>
                <img src={KanbanBoard} alt="kanban board" className="w-auto h-60 lg:h-80 mx-auto" />
-               <h3 className="font-extrabold text-4xl">Kanban Board</h3>
+               <h3 className="font-bold text-4xl">Breco Board Agile</h3>
             </Link>
          </header>
 
